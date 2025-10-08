0c3bd846568fec3e5743e588d06bc0d294b60264
Anushka A. Bhasme <78693700+https-ab@users.noreply.github.com>
2025-10-08 10:28:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
 //your JS code here. If required.
+let form = document.getElementById("form");
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+let checkbox = document.getElementById("checkbox");
+let existing = document.getElementById("existing");
+
+let savedUsername = localStorage.getItem("username");
+let savedPassword = localStorage.getItem("password");
+
+if(savedPassword && savedUsername) {
+	existing.style.display = "block";
+}
+
+form.addEventListener("submit", function(e) {
+	e.preventDefault();
+
+	let name = username.value.trim();
+	let pass = password.value.trim();
+
+	if(!name || !pass ) {
+		return;
+	}
+
+	alert(`Logged in as ${name}`);
+
+	if(checkbox.checked) {
+		localStorage.setItem("name", name);
+		localStorage.setItem("password", pass);
+		existing.style.display = "block";
+	} else {
+		localStorage.removeItem("name");
+		localStorage.removeItem("password");
+		existing.style.display = "none";
+	}
+	form.reset();
+})
+
+existing.addEventListener("click", function() {
+	let storedName = localStorage.getItem("name");
+	if (storedName) {
+		alert(`Logged in as ${storedName}`);
+	}
+})
